b1789c9097525985ccf059eec6efd4623210712b
KerenBermeo <bermeokeren@gmail.com>
2024-06-10 14:35:54 -0500

    Verificar la conexión

--- db/database.py
+++ db/database.py
@@ -9,3 +9,12 @@ mongodb_uri = os.getenv('MONGODB_URI').strip('"')
 client = MongoClient(mongodb_uri)
 db = client["url_shortener"]
 collection = db["urls"]
+
+
+# Verificar la conexión consultando una colección
+try:
+    # Intentar contar documentos en la colección
+    count = collection.count_documents({})
+    print(f"Conexión exitosa a MongoDB.")
+except Exception as e:
+    print("Error al conectar a la colección 'urls' en MongoDB:", e)
